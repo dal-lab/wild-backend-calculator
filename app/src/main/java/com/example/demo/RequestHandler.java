cf2110b283eb4bd91798e6177daa34f48e7fb960
jihwooon <jihwooon@gmail.com>
2024-10-01 21:34:30 +0900

    HTTP 응답 처리 방식 개선
    RequestHandler 클래스의 HTTP 응답 처리 방식이 변경되었습니다. 이전에는 요청 키만을 반환하였지만, 이제는 "Hello World"라는 메시지를 반환합니다.
    
    주요 변경사항:
    1. handle 메소드에서 "Hello World" 문자열을 생성하고, 이를 sendResponse 메소드를 통해 클라이언트에게 반환합니다.
    2. sendResponse 메소드가 추가되었습니다. 이 메소드는 HTTP 응답 헤더를 설정하고, 응답 본문을 작성하여 클라이언트에게 반환합니다.

--- app/src/main/java/com/example/demo/RequestHandler.java
+++ app/src/main/java/com/example/demo/RequestHandler.java
@@ -4,14 +4,26 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URI;
 
 public class RequestHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        getRequestKey(exchange);
+        String hello = "hello World";
+        String requestKey = getRequestKey(exchange);
 
-        exchange.sendResponseHeaders(200, 0);
+        sendResponse(exchange, hello);
+    }
+
+    private static void sendResponse(HttpExchange exchange, String responseContent) throws IOException {
+        byte[] bytes = responseContent.getBytes();
+
+        exchange.sendResponseHeaders(200, bytes.length);
+
+        try (OutputStream outputStream = exchange.getResponseBody()) {
+            outputStream.write(bytes);
+        }
     }
 
     private String getRequestKey(HttpExchange exchange) {
