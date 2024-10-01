9087179584d5982768b1ed7e7a8479ac2fbfd720
GUAJEGUICHAN <qkr135qkr@gmail.com>
2024-10-02 01:01:54 +0900

    단순 계산기 API 기능 구현

--- app/src/main/java/com/example/demo/App.java
+++ app/src/main/java/com/example/demo/App.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.URI;
@@ -19,9 +20,39 @@ public class App {
             String method = exchange.getRequestMethod();
             URI uri = exchange.getRequestURI();
             String path = uri.getPath();
-            System.out.println("method: " + method + " uri: " + uri + " path: " + path);
+            String key = method + " " + path;
+            System.out.println("key: " + key);
+            String response ="";
 
-            String response = "Hello, World! \n";
+            if(key.equals("GET /")) {
+                response = "Hello World\n";
+            } else if(key.equals("POST /calculations")) {
+                InputStream inputStream = exchange.getRequestBody();
+                String requestBody = new String(inputStream.readAllBytes());
+                String[] splitedRequestBody = requestBody.split(" ");
+                System.out.println("requestBody: " + requestBody);
+                int number1 = Integer.parseInt(splitedRequestBody[0]);
+                int number2 = Integer.parseInt(splitedRequestBody[2]);
+                String operation = splitedRequestBody[1];
+
+                if(operation.equals("+")) {
+                    response = String.valueOf(number1 + number2);
+                }
+                else if(operation.equals("-")) {
+                    response = String.valueOf(number1 - number2);
+                }
+                else if(operation.equals("*")) {
+                    response = String.valueOf(number1 * number2);
+                }
+                else if(operation.equals("/")) {
+                    response = String.valueOf(number1 / number2);
+                }
+                else {
+                    response = "Error";
+                }
+
+            }
+            response += "\n";
             exchange.sendResponseHeaders(200, response.length());
             try (OutputStream outputStream = exchange.getResponseBody()) {
                 outputStream.write(response.getBytes());
