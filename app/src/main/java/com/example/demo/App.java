fc2ec399a6c46c27355f7318010e1d4053342859
GUAJEGUICHAN <qkr135qkr@gmail.com>
2024-10-02 00:30:21 +0900

    main 내에 사용 코드를 run()에 옮겨서 분리

--- app/src/main/java/com/example/demo/App.java
+++ app/src/main/java/com/example/demo/App.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 public class App {
     public static void main(String[] args) {
+     App app = new App();
+     app.run();
+    }
+    private void run() {
         System.out.println("Hello, world!");
     }
 }
