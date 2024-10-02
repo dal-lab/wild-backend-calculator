5bcbbf83d6213b0b04aebfa8cbdf6bb2d769a0df
jihwooon <jihwooon@gmail.com>
2024-10-02 21:01:16 +0900

    계산기 기능에 사칙연산 추가
    PostCalculationRequest 핸들러에서 단순 덧셈만 지원하던 기능을 사칙연산을 지원하도록 확장하였습니다.
    
    주요 변경사항:
    1. 입력 문자열을 파싱하여 연산자를 추출하도록 변경하였습니다.
    2. 추출한 연산자에 따라 덧셈, 뺄셈, 곱셈, 나눗셈을 수행하도록 switch 문을 추가하였습니다.
    3. 지원하지 않는 연산자가 입력되었을 경우 IllegalArgumentException을 발생시키도록 하였습니다.

--- app/src/main/java/com/example/demo/presentation/PostCalculationRequest.java
+++ app/src/main/java/com/example/demo/presentation/PostCalculationRequest.java
@@ -5,9 +5,22 @@ public class PostCalculationRequest implements RequestMethodHandler {
     @Override
     public String handler(String content) {
         String[] values = content.split(" ");
+        String operator = String.valueOf(values[1]);
         int number1 = Integer.parseInt(values[0]);
         int number2 = Integer.parseInt(values[2]);
 
-        return String.valueOf(number1 + number2);
+        switch (operator) {
+            case "+":
+                return String.valueOf(number1 + number2);
+            case "-":
+                return String.valueOf(number1 - number2);
+            case "*":
+                return String.valueOf(number1 * number2);
+            case "/":
+                return String.valueOf(number1 / number2);
+            default:
+                throw new IllegalArgumentException(
+                        "지원하지 않는 연산자입니다: " + operator);
+        }
     }
 }
