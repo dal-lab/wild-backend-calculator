98bb11d24e126ff86aa16be2f1336adcf5e7c521
GUAJEGUICHAN <qkr135qkr@gmail.com>
2024-10-03 12:47:52 +0900

    OperationMultiply 계산식 정상화

--- app/src/main/java/com/example/demo/application/OperatorMultiply.java
+++ app/src/main/java/com/example/demo/application/OperatorMultiply.java
@@ -3,6 +3,6 @@ package com.example.demo.application;
 public class OperatorMultiply implements Operator {
     @Override
     public int calculate(int number1, int number2) {
-        return 0;
+        return number1 * number2;
     }
 }
