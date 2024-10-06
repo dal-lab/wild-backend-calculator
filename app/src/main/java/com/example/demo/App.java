ec588a2f7e642e4f90860e1ae5f915ab89ea9670
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-10-06 19:40:07 +0900

    HTTP 1.1 서버 기본 설정

--- app/src/main/java/com/example/demo/App.java
+++ app/src/main/java/com/example/demo/App.java
@@ -1,7 +1,19 @@
 package com.example.demo;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) throws IOException {
+        App app = new App();
+        app.run();
+    }
+    public void run() throws IOException {
+        InetSocketAddress address = new InetSocketAddress("localhost",8080);
+        HttpServer httpServer = HttpServer.create(address, 0);
+        httpServer.start();
+        System.out.println("Listening on http://localhost:8080");
     }
 }
