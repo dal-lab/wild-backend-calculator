a551a95aec2f4c4b9fcea7dc742eaaaae7a08136
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-10-09 22:24:02 +0900

    Main App -> @SpringBootApplication 으로 변환

--- app/src/main/java/com/example/demo/App.java
+++ app/src/main/java/com/example/demo/App.java
@@ -1,21 +1,16 @@
 package com.example.demo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 
-@Configuration
-@ComponentScan(basePackages = {"com.example.demo"})
+@SpringBootApplication
 public class App {
     public static void main(String[] args) throws IOException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
-        Server server = context.getBean(Server.class);
-        server.run();
+        SpringApplication.run(App.class, args);
     }
 
     @Bean
