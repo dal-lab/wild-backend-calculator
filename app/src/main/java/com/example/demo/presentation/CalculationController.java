fc7410f15d95327c30fd9af767f493bbc8dba9e2
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-10-10 00:34:18 +0900

    핸들러 처리 -> Spring 컨트롤러로 전환

---
+++ app/src/main/java/com/example/demo/presentation/CalculationController.java
@@ -0,0 +1,50 @@
+package com.example.demo.presentation;
+
+import com.example.demo.application.Calculator;
+import com.example.demo.infrastructure.Calculation;
+import com.example.demo.presentation.dto.CalculationListResponseDto;
+import com.example.demo.presentation.dto.CalculationRequestDto;
+import com.example.demo.presentation.dto.CalculationResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/calculations")
+public class CalculationController {
+
+    private final Calculator calculator;
+
+    public CalculationController(Calculator calculator) {
+        this.calculator = calculator;
+    }
+
+    @GetMapping
+    public CalculationListResponseDto list() {
+        List<Calculation> calculations = calculator.getCalculationList();
+
+        return CalculationListResponseDto.of(calculations);
+    }
+
+    @PostMapping
+    public CalculationResponseDto create(
+            @RequestBody
+            CalculationRequestDto requestDto
+    ) {
+        Calculation calculation = calculator.calculate(
+                requestDto.getNumber1(),
+                requestDto.getNumber2(),
+                requestDto.getOperator());
+
+        return new CalculationResponseDto(
+                calculation.getNumber1(),
+                calculation.getNumber2(),
+                calculation.getOperator(),
+                calculation.getResult()
+        );
+    }
+}
