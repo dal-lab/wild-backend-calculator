e0812241fb71598103448100fc93629074d02463
GUAJEGUICHAN <qkr135qkr@gmail.com>
2024-10-09 17:37:31 +0900

    사칙연산 테스트 코드 작성

--- app/src/test/java/com/example/demo/AppTest.java
+++ app/src/test/java/com/example/demo/AppTest.java
@@ -1,13 +1,44 @@
 package com.example.demo;
 
+import com.example.demo.application.Calculator;
+import com.example.demo.infrastructure.Calculation;
+import com.example.demo.infrastructure.CalculationRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class AppTest {
+class CalculatorTest {
+    CalculationRepository calculationRepository;
+    Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculationRepository = new CalculationRepository();
+        calculator = new Calculator(calculationRepository);
+    }
+
     @Test
-    void creation() {
-        App app = new App();
-        assertThat(app).isNotNull();
+    void calculateAddition() {
+        Calculation calculation = calculator.calculate(1, 2, "+");
+        assertThat(calculation.getResult()).isEqualTo(3);
+    }
+
+    @Test
+    void calculateSubtraction() {
+        Calculation calculation = calculator.calculate(5, 3, "-");
+        assertThat(calculation.getResult()).isEqualTo(2);
+    }
+
+    @Test
+    void calculateMultiplication() {
+        Calculation calculation = calculator.calculate(4, 3, "*");
+        assertThat(calculation.getResult()).isEqualTo(12);
+    }
+
+    @Test
+    void calculateDivision() {
+        Calculation calculation = calculator.calculate(8, 2, "/");
+        assertThat(calculation.getResult()).isEqualTo(4);
     }
 }
