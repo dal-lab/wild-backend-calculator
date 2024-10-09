145cbd26c4b70bce8c586885ee12d4694ee6d0b5
GUAJEGUICHAN <qkr135qkr@gmail.com>
2024-10-09 11:43:33 +0900

    spring-setup: App.java에 ApplicationContext 생성

--- app/src/main/java/com/example/demo/App.java
+++ app/src/main/java/com/example/demo/App.java
@@ -9,11 +9,20 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.URI;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class App {
     public static void main(String[] args) throws IOException {
-     App app = new App();
-     app.run();
+
+        // ApplicationContext 생성 방식중에서 Annotation 방식을 사용하여 생성
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        App app = context.getBean(App.class);
+        app.run();
     }
+
     private void run() throws IOException {
         HttpServer httpServer = HttpServer.create(new InetSocketAddress(8080), 0);
         httpServer.createContext("/", new RequestHandler());
