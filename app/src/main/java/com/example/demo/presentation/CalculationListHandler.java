5b4fd7c132102a43c66a4dd9375b846cf0a0115f
GUAJEGUICHAN <qkr135qkr@gmail.com>
2024-10-09 15:00:46 +0900

    CalculationCreateResource, CalculationListResource -> ~Hander로 이름 변경

--- app/src/main/java/com/example/demo/presentation/CalculationListHandler.java
+++ app/src/main/java/com/example/demo/presentation/CalculationListHandler.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-public class CalculationListResource extends ResourceMethodHandler {
+public class CalculationListHandler extends ResourceMethodHandler {
     private final Calculator calculator = new Calculator();
     private final ObjectMapper objectMapper = new ObjectMapper();
 
