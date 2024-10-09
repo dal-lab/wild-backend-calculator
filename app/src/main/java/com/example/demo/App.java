fa228357a6183471ca9e55411f41475f3caa9d88
GUAJEGUICHAN <qkr135qkr@gmail.com>
2024-10-09 11:43:33 +0900

    spring-setup: App.java에 ApplicationContext 생성

--- app/src/main/java/com/example/demo/App.java
+++ app/src/main/java/com/example/demo/App.java
@@ -1,7 +1,23 @@
 package com.example.demo;
 
+import com.example.demo.presentation.RequestHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URI;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) throws IOException {
+     App app = new App();
+     app.run();
+    }
+    private void run() throws IOException {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(8080), 0);
+        httpServer.createContext("/", new RequestHandler());
+        httpServer.start();
+        System.out.println("Server started. Listening on " + httpServer.getAddress());
     }
 }
