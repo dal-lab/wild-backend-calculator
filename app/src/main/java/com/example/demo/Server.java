90b4f05ce892ebe4df835f2db078d834728e3655
GUAJEGUICHAN <qkr135qkr@gmail.com>
2024-10-09 12:07:57 +0900

    requestHandler를 멤버 변수로 사용

--- app/src/main/java/com/example/demo/Server.java
+++ app/src/main/java/com/example/demo/Server.java
@@ -1,16 +1,25 @@
 package com.example.demo;
 
 import com.example.demo.presentation.RequestHandler;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
+
 public class Server {
+    private final HttpHandler requestHandler;
+
+    public Server(HttpHandler requestHandler) {
+        this.requestHandler = requestHandler;
+    }
+
     public void run() throws IOException {
         HttpServer httpServer = HttpServer.create(new InetSocketAddress(8080), 0);
-        httpServer.createContext("/", new RequestHandler());
+        httpServer.createContext("/", requestHandler);
         httpServer.start();
         System.out.println("Server started. Listening on " + httpServer.getAddress());
     }
+
 }
