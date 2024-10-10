45ca15c6f8116ffea2bcc8ea6707826fbd0764a2
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-10-10 11:57:02 +0900

    Revert "계산 결과 데이터 저장 및 Dto로 반환"
    This reverts commit 9f9536b82e395e1b923cab4c73f96c5c23768ee2.

---
+++ app/src/main/java/com/example/demo/presentation/CalculationCreateResource.java
@@ -0,0 +1,36 @@
+package com.example.demo.presentation;
+
+import com.example.demo.application.Calculator;
+import com.example.demo.presentation.dto.CalculationRequestDto;
+import com.example.demo.presentation.dto.CalculationResponseDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class CalculationCreateResource extends ResourceMethodHandler {
+    public final static String KEY = "POST /calculations";
+
+    private final Calculator calculator = new Calculator();
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public String handle(String content) throws JsonProcessingException {
+
+
+        CalculationRequestDto requestDto =
+                objectMapper.readValue(content, CalculationRequestDto.class);
+
+        int result = calculator.calculate(
+                requestDto.getNumber1(),
+                requestDto.getNumber2(),
+                requestDto.getOperator()
+        );
+        return objectMapper.writeValueAsString(
+                new CalculationResponseDto(
+                        requestDto.getNumber1(),
+                        requestDto.getNumber2(),
+                        requestDto.getOperator(),
+                        result
+                ));
+
+    }
+}
