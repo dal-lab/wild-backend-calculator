75bb6939f1072f9478a475bd6e8c9ca046001dff
GUAJEGUICHAN <qkr135qkr@gmail.com>
2024-10-10 23:37:47 +0900

    contextLoads 정상 테스트

--- app/src/test/java/com/example/demo/AppTest.java
+++ app/src/test/java/com/example/demo/AppTest.java
@@ -1,44 +1,16 @@
 package com.example.demo;
 
-import com.example.demo.application.Calculator;
-import com.example.demo.infrastructure.Calculation;
-import com.example.demo.infrastructure.FakeCalculationRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
-class CalculatorTest {
-    FakeCalculationRepository calculationRepository;
-    Calculator calculator;
-
-    @BeforeEach
-    void setUp() {
-        calculationRepository = new FakeCalculationRepository();
-        calculator = new Calculator(calculationRepository);
-    }
-
+@SpringBootTest
+class AppTest {
     @Test
-    void calculateAddition() {
-        Calculation calculation = calculator.calculate(1, 2, "+");
-        assertThat(calculation.getResult()).isEqualTo(3);
+    void contextLoads() {
+
     }
 
-    @Test
-    void calculateSubtraction() {
-        Calculation calculation = calculator.calculate(5, 3, "-");
-        assertThat(calculation.getResult()).isEqualTo(2);
-    }
 
-    @Test
-    void calculateMultiplication() {
-        Calculation calculation = calculator.calculate(4, 3, "*");
-        assertThat(calculation.getResult()).isEqualTo(12);
-    }
-
-    @Test
-    void calculateDivision() {
-        Calculation calculation = calculator.calculate(8, 2, "/");
-        assertThat(calculation.getResult()).isEqualTo(4);
-    }
 }
