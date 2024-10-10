7128590408b9d16a4618c2db4bd520a948d8fcef
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-10-10 10:24:57 +0900

    Calculator 테스트 코드

---
+++ app/src/test/java/com/example/demo/CalculatorTest.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+import com.example.demo.application.Calculator;
+import com.example.demo.infrastructure.Calculation;
+import com.example.demo.infrastructure.FakeCalculationRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp(){
+        FakeCalculationRepository calculationRepository = new FakeCalculationRepository();
+        calculator = new Calculator(calculationRepository);
+    }
+    @Test
+    void plus() {
+        Calculation calculation = calculator.calculate(2,1,"+");
+        assertThat(calculation.getResult()).isEqualTo(3);
+    }
+    @Test
+    void minus() {
+        Calculation calculation = calculator.calculate(2,1,"-");
+        assertThat(calculation.getResult()).isEqualTo(1);
+    }
+    @Test
+    void divide() {
+        Calculation calculation = calculator.calculate(2,1,"*");
+        assertThat(calculation.getResult()).isEqualTo(2);
+    }
+    @Test
+    void multiply() {
+        Calculation calculation = calculator.calculate(2,1,"/");
+        assertThat(calculation.getResult()).isEqualTo(2);
+    }
+}
