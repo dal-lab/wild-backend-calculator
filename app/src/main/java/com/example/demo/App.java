d130fce90b3426b14991a6738f21fc1d3342ccd6
GUAJEGUICHAN <qkr135qkr@gmail.com>
2024-10-10 22:42:05 +0900

    SpringApplication 사용 및 SpringBootApplication Annotation 적용

--- app/src/main/java/com/example/demo/App.java
+++ app/src/main/java/com/example/demo/App.java
@@ -1,23 +1,16 @@
 package com.example.demo;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
+@SpringBootApplication
 public class App {
     public static void main(String[] args) throws IOException {
-        // ApplicationContext 생성 방식중에서 Annotation 방식을 사용하여 생성
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
-        Server server = context.getBean(Server.class);
-        server.run();
+        SpringApplication.run(App.class, args);
     }
 
     @Bean
