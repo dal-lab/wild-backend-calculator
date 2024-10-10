9253681abd7731aaacc9553129dcb1f9d93095a9
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-10-10 10:51:54 +0900

    mock -> CalculationRepository 테스트

--- app/src/test/java/com/example/demo/application/CalculatorTest.java
+++ app/src/test/java/com/example/demo/application/CalculatorTest.java
@@ -1,40 +1,50 @@
 package com.example.demo.application;
+
 import com.example.demo.infrastructure.Calculation;
-import com.example.demo.infrastructure.FakeCalculationRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class CalculatorTest {
     private Calculator calculator;
     private CalculationRepository calculationRepository;
 
     @BeforeEach
-    void setUp(){
-        CalculationRepository calculationRepository = mock(CalculationRepository.class);
+    void setUp() {
+        calculationRepository = mock(CalculationRepository.class);
         calculator = new Calculator(calculationRepository);
     }
 
     @Test
     void plus() {
-        Calculation calculation = calculator.calculate(2,1,"+");
+        Calculation calculation = calculator.calculate(2, 1, "+");
         assertThat(calculation.getResult()).isEqualTo(3);
+        verify(calculationRepository).add(any(Calculation.class));
     }
+
     @Test
     void minus() {
-        Calculation calculation = calculator.calculate(2,1,"-");
+        Calculation calculation = calculator.calculate(2, 1, "-");
         assertThat(calculation.getResult()).isEqualTo(1);
+        verify(calculationRepository).add(any(Calculation.class));
     }
+
     @Test
     void divide() {
-        Calculation calculation = calculator.calculate(2,1,"*");
+        Calculation calculation = calculator.calculate(2, 1, "*");
         assertThat(calculation.getResult()).isEqualTo(2);
+        verify(calculationRepository).add(any(Calculation.class));
     }
+
     @Test
     void multiply() {
-        Calculation calculation = calculator.calculate(2,1,"/");
+        Calculation calculation = calculator.calculate(2, 1, "/");
         assertThat(calculation.getResult()).isEqualTo(2);
+        verify(calculationRepository).add(any(Calculation.class));
     }
+    
 }
