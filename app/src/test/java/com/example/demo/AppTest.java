6579fb952784c32b873836095df48f9107ba2eda
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-10-10 10:24:12 +0900

    Application 테스트 코드

--- app/src/test/java/com/example/demo/AppTest.java
+++ app/src/test/java/com/example/demo/AppTest.java
@@ -1,42 +1,12 @@
 package com.example.demo;
 
-import com.example.demo.application.Calculator;
-import com.example.demo.infrastructure.Calculation;
-import com.example.demo.infrastructure.FakeCalculationRepository;
-import com.example.demo.infrastructure.InMemoryCalculationRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+@SpringBootTest
+public class AppTest {
+    @Test
+    void contextLoads(){
 
-class AppTest {
-    FakeCalculationRepository calculationRepository;
-    Calculator calculator;
-
-    @BeforeEach
-    void setUp(){
-        calculationRepository = new FakeCalculationRepository();
-        calculator = new Calculator(calculationRepository);
-    }
-    @Test
-    void plus() {
-        Calculation calculation = calculator.calculate(2,1,"+");
-        assertThat(calculation.getResult()).isEqualTo(3);
-    }
-    @Test
-    void minus() {
-        Calculation calculation = calculator.calculate(2,1,"-");
-        assertThat(calculation.getResult()).isEqualTo(1);
-    }
-    @Test
-    void divide() {
-        Calculation calculation = calculator.calculate(2,1,"*");
-        assertThat(calculation.getResult()).isEqualTo(2);
-    }
-    @Test
-    void multiply() {
-        Calculation calculation = calculator.calculate(2,1,"/");
-        assertThat(calculation.getResult()).isEqualTo(2);
     }
 }
