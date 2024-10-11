ae33a51585d3c867eb00c740361f2dc0b83605dc
GUAJEGUICHAN <qkr135qkr@gmail.com>
2024-10-11 21:59:03 +0900

    @@MockBean으로 calculator을 Mocking합니다. anyInt, any로 변수를 넣습니다.

--- app/src/test/java/com/example/demo/presentation/CalculationControllerTest.java
+++ app/src/test/java/com/example/demo/presentation/CalculationControllerTest.java
@@ -1,12 +1,18 @@
 package com.example.demo.presentation;
 
+import com.example.demo.application.Calculator;
+import com.example.demo.infrastructure.Calculation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -19,6 +25,9 @@ class CalculationControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private Calculator calculator;
+
     void list() throws Exception {
         mockMvc.perform(get("/calculations"))
                 .andExpect(status().isOk())
@@ -26,6 +35,7 @@ class CalculationControllerTest {
     }
 
     void create() throws Exception {
+        when(calculator.calculate(anyInt(), anyInt(), any())).thenReturn(new Calculation(1, 2, "+", 3));
         String json = "{\"left\": 1, \"right\": 2, \"operator\": \"+\"}";
         String result = "{\"left\":1,\"right\":2,\"operator\":\"+\",\"result\":3}";
         mockMvc.perform(post("/calculations")
